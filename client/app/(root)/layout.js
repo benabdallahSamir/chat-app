f3b16491b7944c3eac18988947ac4c94c7bbe45f
= <samirbenabdallah4002@gmail.com>
2025-01-24 15:08:51 +0100

    handle project

--- client/app/(root)/layout.js
+++ client/app/(root)/layout.js
@@ -1,8 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { ToastContainer } from "react-toastify";
 import AsideBar from "@/components/AsideBar";
 import Navbar from "@/components/Navbar";
+import ReduxContainer from "@/redux/ReduxContainer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,13 +23,15 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white text-black w-screen min-h-screen flex">
-        <AsideBar />
-        <main className="w-4/5 border border-l h-screen border-gray-200 flex flex-col">
-          <Navbar />
-          {children}
-        </main>
-        <ToastContainer />
+      <body className="bg-white text-black w-screen min-h-screen flex overflow-hidden">
+        <ReduxContainer>
+          <AsideBar />
+          <main className="w-4/5 border border-l h-screen border-gray-200">
+            <Navbar />
+            {children}
+          </main>
+          <ToastContainer />
+        </ReduxContainer>
       </body>
     </html>
   );
